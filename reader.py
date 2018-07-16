3dc1ad0b6c66e6618839f967acad57c0088beaf0
Guilherme Lucas <guilherme.slucas@gmail.com>
2018-07-16 15:49:57 -0300

    adding translation

--- reader.py
+++ reader.py
@@ -1,14 +1,14 @@
-import requests, json  
-
-endpoint = 'https://westus.api.cognitive.microsoft.com/vision/v1.0'  
-api_key = ''
-
-headers = { 
-    'Content-Type': 'application/json',  
-    'Ocp-Apim-Subscription-Key': api_key,  
-}
+import requests, json, http.client, urllib.parse, uuid
 
 def submitImageText(image_url):
+    endpoint = 'https://westus.api.cognitive.microsoft.com/vision/v1.0'  
+    api_key = ''
+
+    headers = { 
+        'Content-Type': 'application/json',  
+        'Ocp-Apim-Subscription-Key': api_key,  
+    }
+
     body = {'url': image_url}
 
     params = {'handwriting' : 'false'}
@@ -30,5 +30,38 @@ def submitImageText(image_url):
 
     return full_text
 
+
+def call_translate(content):
+    subscriptionKey = ''
+
+    host = 'api.cognitive.microsofttranslator.com'
+    
+    path = '/translate?api-version=3.0'
+
+    params = "&to=pt-br"
+
+    headers = {
+        'Ocp-Apim-Subscription-Key': subscriptionKey,
+        'Content-type': 'application/json',
+        'X-ClientTraceId': str(uuid.uuid4())
+    }
+
+    conn = http.client.HTTPSConnection(host)
+    conn.request ("POST", path + params, content, headers)
+    response = conn.getresponse()
+    return response.read()
+
+def translate(text):
+    requestBody = [{
+        'Text' : text,
+    }]
+
+    content = json.dumps(requestBody, ensure_ascii=False).encode('utf-8')
+    result = call_translate(content)
+
+    print(json.loads(result.decode('utf-8'))[0]['translations'][0]['text'])
+
+
 if __name__ == '__main__':
-    print(submitImageText('http://doc.qt.io/archives/qt-4.8/images/qml-textselection-example.png'))
+    extracted_text = submitImageText('http://www.sunywcc.edu/cms/wp-content/uploads/2018/01/Slogan-Samples-04-300x188.jpg')
+    translate(extracted_text)
