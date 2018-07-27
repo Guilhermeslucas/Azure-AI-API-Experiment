3766971721c2727db290e5208653c36b6a6d6fca
Guilherme Lucas <guilherme.slucas@gmail.com>
2018-07-26 23:57:10 -0300

    changing the ordering

--- back_end/server.py
+++ back_end/server.py
@@ -9,7 +9,7 @@ CORS(app)
 
 @app.route("/") 
 def hello(): 
-	return "Hello World!" 
+	return "Hello World! I'm the updated version!" 
 
 @app.route("/api/submit", methods=['POST']) 
 def process():
