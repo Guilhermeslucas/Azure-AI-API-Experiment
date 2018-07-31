5adcbbe268a11df51cff6ec32fd200ae3ae601ae
Guilherme Lucas <guilherme.slucas@gmail.com>
2018-07-30 23:39:58 -0300

    changing text

--- back_end/server.py
+++ back_end/server.py
@@ -9,7 +9,7 @@ CORS(app)
 
 @app.route("/") 
 def hello(): 
-	return "Hello World! I'm the updated version!" 
+	return "Hello World!" 
 
 @app.route("/api/submit", methods=['POST']) 
 def process():
